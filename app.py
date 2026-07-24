dbf99032139f2a874e46b336700dbad5f63a07b1
Christian-233 <chgo4693@colorado.edu>
2026-07-24 16:51:59 +0000

    created database

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import psycopg2
 
-conn = psycopg2.connect("postgresql://db_3308_render_database_user:h9LRgUEYuV3Wuws6YX2rLBl62LOLhOgR@dpg-d9hor0d7vvec73ervrj0-a/db_3308_render_database")
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,5 +16,38 @@ def db_test():
     except Exception as e:
         return f"Database connection failed: {e}"
     finally:
+        if conn is not None:
+            conn.close()
+
+@app.route("/db_create")
+def db_create():
+    conn = None
+    cur = None
+
+    try:
+        conn = psycopg2.connect("postgresql://db_3308_render_database_user:h9LRgUEYuV3Wuws6YX2rLBl62LOLhOgR@dpg-d9hor0d7vvec73ervrj0-a/db_3308_render_database")
+        cur = conn.cursor()
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS Basketball(
+                First varchar(255),
+                Last varchar(255),
+                City varchar(255),
+                Name varchar(255),
+                Number int
+            );
+        """)
+
+        conn.commit()
+        return "Basketball Table Created"
+
+    except Exception as e:
+        if conn is not None:
+            conn.rollback()
+        return f"Database error: {e}"
+
+    finally:
+        if cur is not None:
+            cur.close()
         if conn is not None:
             conn.close()
